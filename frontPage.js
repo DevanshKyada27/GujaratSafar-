5e026daf2c3ee71423672c2962e851b5298154ad
Devansh Kyada <devansh.kyada27@gmail.com>
2025-03-19 12:17:04 +0530

    Added top & bottom section

---
+++ frontPage.js
@@ -0,0 +1,51 @@
+//typed js
+var typed = new Typed('#element', {
+    strings: ['Ahmedabad (અમદાવાદ)', 'Vadodara (વડોદરા)', 'Gandhinagar (ગાંધીનગર)', 'Surat (સુરત)', 'Dwarka (દ્વારકા)', 'Somnath (સોમનાથ)', 'Kutch (કચ્છ)', 'Rajkot (રાજકોટ)', 'and many more....'],
+    typeSpeed: 120,
+    loop: true
+});
+
+
+//Moving background photo
+const slides = document.querySelectorAll(".img-slides"); // Select all slides
+let currentSlide = 0;
+// Function to change the active slide with a sliding effect
+const changeSlide = () => {
+    slides.forEach((slide, index) => {
+        // Reset all slides to the right
+        slide.style.transform = `translateX(${(index - currentSlide) * 100}%)`;
+    });
+    currentSlide = (currentSlide + 1) % slides.length; // Move to the next slide
+}   
+// Initialize the slide positions
+slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${index * 100}%)`;
+})  
+setInterval(changeSlide, 3000); // Change slide every 3 seconds
+
+//on click selector for the budget and the plan
+document.addEventListener("DOMContentLoaded", function () {
+    // Budget option click handler
+    document.querySelectorAll('.budget-container div').forEach(option => {
+        option.addEventListener('click', function () {
+            selectOption(this, '.budget-container div');
+        });
+    });
+
+    // Plan option click handler
+    document.querySelectorAll('.plan-container div').forEach(option => {
+        option.addEventListener('click', function () {
+            selectOption(this, '.plan-container div');
+        });
+    });
+
+    // Common function for selection
+    function selectOption(element, selector) {
+        document.querySelectorAll(selector).forEach(option => {
+            option.classList.remove('selected');
+        });
+        element.classList.add('selected');
+    }
+});
+
+
